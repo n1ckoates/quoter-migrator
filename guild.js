5e65f05da101233c723031ef2605e835a9f4a0ce
Nicholas Christopher <nchristopher@tuta.io>
2021-08-27 13:00:01 -0700

    📄 Use alternate license disclaimer
    - Used license disclaimer with line breaks.
    - Added license disclaimer to guild.js.

--- guild.js
+++ guild.js
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2021 Nicholas Christopher
+
+This file is part of Quoter Migrator.
+
+Quoter Migrator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+Quoter Migrator is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Quoter.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const { Schema, model } = require("mongoose");
 
 const guildSchema = new Schema({
--- migrate.js
+++ migrate.js
@@ -1,11 +1,19 @@
 /*
 Copyright (C) 2021 Nicholas Christopher
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+This file is part of Quoter Migrator.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+Quoter Migrator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
 
-You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+Quoter Migrator is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Quoter.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 const quickDB = require("quick.db");
